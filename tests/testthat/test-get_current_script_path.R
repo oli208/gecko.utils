9868ef36f7e6c7d1b5f7744859ae7bb3b8b6de16
oli208 <git-oliver@web.de>
2025-01-10 15:13:28 +0100

    remove package declaration

--- tests/testthat/test-get_current_script_path.R
+++ tests/testthat/test-get_current_script_path.R
@@ -1,14 +1,15 @@
 library(testthat)
 library(gecko.utils)
+
 test_that("get_current_script_path returns the full path in RStudio or interactive mode", {
     # Assuming we're in RStudio or in a similar interactive session
-    result <- gecko.utils:::get_current_script_path()
+    result <- get_current_script_path()
     expect_type(result, "character")
     expect_true(file.exists(result) || is.null(result))
 })
 
 test_that("get_current_script_path returns only the filename when only_filename = TRUE", {
-    result <- gecko.utils:::get_current_script_path(only_filename = TRUE)
+    result <- get_current_script_path(only_filename = TRUE)
     expect_type(result, "character")
 
     # Check that the result does not contain directory paths
